c67a069b666627fac33e5376742bc39ab538927d
belaidimohamed <mohamedbelaidi408@gmail.com>
2021-12-19 18:51:04 +0100

    header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+     Task 
+    </div>
+  )
+}
+
+export default header
---
+++ src/views/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+function home() {
+  return (
+    <div>
+      <h1>Task tracker</h1>
+      <Button variant="contained" color="success"> Add </Button>
+    </div>
+  )
+}
+
+export default home
